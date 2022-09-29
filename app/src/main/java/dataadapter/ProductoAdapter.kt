a0c1b324c375be338fc4360e3fd0f6bf242d4cb1
davidquintr <davidquintr97@gmail.com>
2022-09-29 00:14:32 -0600

    implements

--- app/src/main/java/dataadapter/ProductoAdapter.kt
+++ app/src/main/java/dataadapter/ProductoAdapter.kt
@@ -10,14 +10,31 @@ import ni.edu.uca.listadoprod.databinding.ActivityMainBinding
 import ni.edu.uca.listadoprod.databinding.ItemlistaBinding
 
 
-class ProductoAdapter(val listProd : List<Producto>): RecyclerView.Adapter<ProductoAdapter.ProductoHolder>(){
+class ProductoAdapter(
+    val listProd : List<Producto>,
+    private val onClickListener: (Producto) -> Unit,
+    private val deleteClickListener: (Producto) -> Unit
+    ): RecyclerView.Adapter<ProductoAdapter.ProductoHolder>(){
+
 
     inner class ProductoHolder(val binding: ItemlistaBinding) : RecyclerView.ViewHolder(binding.root){
-        fun cargar(producto: Producto){
+        fun cargar(
+            producto: Producto,
+            onClickListener: (Producto) -> Unit,
+            deleteClickListener: (Producto) -> Unit
+        ){
             with(binding){
                 tvCodProd.text = producto.id.toString()
                 tvNombreProd.text = producto.nombre
-                tvPrecioProd.text = producto.precio.toString()
+                tvPrecioProd.text = "C$ ${producto.precio.toString()}"
+
+                btnEditar.setOnClickListener {
+                    onClickListener(producto)
+                }
+
+                btnEliminar.setOnClickListener {
+                    deleteClickListener(producto)
+                }
             }
         }
     }
@@ -30,6 +47,8 @@ class ProductoAdapter(val listProd : List<Producto>): RecyclerView.Adapter<Produ
     }
 
     override fun onBindViewHolder(holder: ProductoHolder, position: Int) {
-        holder.cargar(listProd[position])
+        holder.cargar(listProd[position],onClickListener,deleteClickListener)
     }
+
 }
+
--- app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
@@ -1,9 +1,11 @@
 package ni.edu.uca.listadoprod
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dataadapter.ProductoAdapter
@@ -11,28 +13,55 @@ import dataclass.Producto
 import ni.edu.uca.listadoprod.databinding.ActivityMainBinding
 import ni.edu.uca.listadoprod.databinding.ItemlistaBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding : ActivityMainBinding
     var listaProd = ArrayList<Producto>()
     private var idAct = 0;
+    private var editMode = false
+    private var editedElement : Producto = Producto(-1,"",0.00)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        val prod = Producto(0,"prueba",123.3)
-        listaProd.add(prod)
+        setContentView(binding.root)
+        binding.etID.setText(idAct.toString())
         iniciar()
     }
 
     private fun iniciar() {
         binding.btnAgregar.setOnClickListener {
-            agregarProd()
+            if(!editMode)
+                agregarProd()
+            else
+                terminarEdicion()
+
         }
         binding.btnLimpiar.setOnClickListener {
             limpiar()
         }
+
+    }
+
+    private fun terminarEdicion(){
+        try {
+        with(binding){
+                var itemSel = listaProd.indexOf(editedElement)
+                editedElement = Producto(editedElement.id,etNombreProd.text.toString(),etPrecio.text.toString().toDouble())
+                listaProd[itemSel] = editedElement
+                rvcLista.adapter = ProductoAdapter(listaProd,
+                {producto ->  onItemClick(producto)},
+                {producto -> onDeleteItem(producto)})
+                etID.setText(idAct.toString())
+                btnAgregar.setText("Agregar")
+            }
+            editMode = false
+            editedElement = Producto(-1,"",0.00)
+        }catch (ex : Exception){
+            Toast.makeText(this@MainActivity,"Error ${ex.toString()}",Toast.LENGTH_LONG).show()
+        }
+
+        limpiar()
     }
 
     private fun limpiar(){
@@ -41,14 +70,6 @@ class MainActivity : AppCompatActivity() {
             etPrecio.setText("")
             etID.requestFocus()
         }
-    }
-
-    private fun editarProd(){
-
-    }
-
-    private fun toggleEdit(id : String){
-
     }
 
     private fun agregarProd(){
@@ -63,11 +84,56 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity,"Error ${ex.toString()}",Toast.LENGTH_LONG).show()
             }
             rvcLista.layoutManager = LinearLayoutManager(this@MainActivity)
-            rvcLista.adapter = ProductoAdapter(listaProd)
+            rvcLista.adapter = ProductoAdapter(listaProd,
+                {producto ->  onItemClick(producto)},
+                {producto -> onDeleteItem(producto)})
 
             limpiar()
             idAct++
             etID.setText(idAct.toString())
+        }
+    }
+
+    private fun onDeleteItem(producto: Producto) {
+        var alerta = AlertDialog.Builder(this)
+        alerta.setMessage("¿Quieres eliminar este item?")
+            .setCancelable(false)
+            .setPositiveButton("Aceptar", DialogInterface.OnClickListener {
+                    dialog, id -> eliminarItem(producto)
+            })
+            // negative button text and action
+            .setNegativeButton("Cancelar", DialogInterface.OnClickListener {
+                    dialog, id ->
+            })
+        val dialogo = alerta.create()
+        dialogo.setTitle("Eliminar item")
+        dialogo.show()
+    }
+
+    private fun eliminarItem(producto: Producto){
+        try{
+            listaProd.remove(producto)
+            binding.rvcLista.adapter = ProductoAdapter(listaProd,
+                {producto ->  onItemClick(producto)},
+                {producto -> onDeleteItem(producto)})
+
+        }catch (ex : Exception){
+            Toast.makeText(this@MainActivity,"Error ${ex.toString()}",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onItemClick(producto: Producto) {
+        try {
+            with(binding){
+                etID.setText(producto.id.toString())
+                etNombreProd.setText(producto.nombre.toString())
+                etPrecio.setText(producto.precio.toString())
+                btnAgregar.setText("Finalizar")
+                editMode = true
+                editedElement = producto
+            }
+        }catch (ex : Exception){
+            Toast.makeText(this@MainActivity,"Error ${ex.toString()}",Toast.LENGTH_LONG).show()
         }
     }
 }
